150c93d95079051fc30f0fe6298b84dd9816a2dd
dbs-2012 <shahdarsh345@gmail.com>
2023-12-03 19:53:24 +0530

    changes made

--- src/components/Test/QuizData.jsx
+++ src/components/Test/QuizData.jsx
@@ -1,22 +1,62 @@
 export const QuizData = [
     {
-        question: "Which language runs in a web browser?",
-        options:[ "Java", "C", "Python", "JavaScript"],
-        answer: 4
-    },
-    {
-        question: "What does CSS stand for?",
-        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        id: 1,
+        question: "Basic proportionality theorem  is also known as?",
+        options:[ "JavaBasic theorem", "Thales Theorem", "Potential theorem", "None of the above"],
         answer: 2
     },
     {
-        question: "What does HTML stand for?",
-        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        id: 2,
+        question: "Which of the following is Thale's Theorem?",
+        options:["The diagonal of rectangles creates two triangles.", "Two straight intersecting lines produces equal opposite angles.", "An angle inscribed in a half-circle will be a right angle.", "The base angles of an isosceles triangle are equal."],
+        answer: 4
+    },
+    {
+        id: 3,
+        question: "If two figures are similar, the corresponding sides are",
+        options: ["Equal", "Congruent", "Proportional", "None of the above"],
+        answer: 3
+    },
+    {
+        id: 4,
+        question: "In similar Polygons, corresponding sides are ___ and corresponding angles are ___.",
+        options: ["congruent, proportional", "proportional, congruent", "congruent, congruent", "proportional, proportional"],
+        answer: 2
+    },
+    {
+        id: 5,
+        question: "If a line divides any two sides of a triangle in the same ratio then the line is ________ to the third side.",
+        options: ["parallel", "perpendicular", "co-incides", "None of the above"],
         answer: 1
     },
     {
-        question: "What year was JavaScript launched?",
-        options: ["1996", "1995", "1994", "none of the above"],
-        answer:2
+        id: 6,
+        question: "ABC is a triangle with AB = 13 cm, BC =14 cm and CA=15 cm. AD and BE are the altitudes from A to B to BC and AC respectively. H is the point of intersection of the AD and BE. Then the ratio of HD/HB=",
+        options: ["3/5", "12/13", "4/5", "5/9"],
+        answer: 1
+    },
+    {
+        id: 7,
+        question: "In △ABC, AB = 3 and AC = 4 cm, and AD is the bisector of ∠A. Then, BD:DC is:",
+        options: ["9:16", "4:3", "3:4", "16:9"],
+        answer: 3
+    },
+    {
+        id: 8,
+        question: "In a ΔABC, D and E are points on the sides AB and AC respectively such that DE || BC. If AD = 4x – 3, AE = 8x – 7, BD = 3x – 1 and CE = 5x – 3, find the value of x.",
+        options: ["5", "3", "6", "1"],
+        answer: 4
+    },
+    {
+        id: 9,
+        question: "In a circle with diameter AC, point B lies on the circumference. If angle ABC is a right angle, which statement about the lengths of the sides is true?",
+        options: ["AB = BC", "AB > BC", "AB < BC", "AB = AC"],
+        answer: 1
+    },
+    {
+        id: 10,
+        question: "A tower of height 24m casts a shadow of 50 m, and at the same time, a girl of height 1.8 m casts a shadow. Find the length of the shadow of the girl.",
+        options: ["3.75 m", "3.5 m", "3.25 m", "3 m"],
+        answer: 1
     },
 ];
